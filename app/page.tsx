984619431754de60099a1e82078557a5b4a8c476
Your Name <nathanirahul49@gmail.com>
2024-10-22 13:19:03 -0400

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-amber-300 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={ `${lusitana.classname}  text-xl  text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={ `${lusitana.className}  text-xl  text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Invoice Management System</strong> This is the example for the{' '}
 
               Next.js Learn Course
