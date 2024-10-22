a01c80a96503dea70da2f518cd084cd06c6ad4b7
Your Name <nathanirahul49@gmail.com>
2024-10-22 13:11:57 -0400

    first commit

---
+++ app/ui/fonts.tsx
@@ -0,0 +1,10 @@
+import '@/app/ui/global.css';
+import { Inter, Lusitana } from 'next/font/google';
+
+import React from "react";
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
