0765dcc323d3bfd80b46abf48bbe21d6e8f21b62
Your Name <nathanirahul49@gmail.com>
2024-10-23 16:16:35 -0400

    auth done

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-    return <p>Customer Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import {fetchFilteredCustomers} from '@/app/lib/data';
+
+
+export default async function Page(props: {
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+    }>;
+}) {
+    const searchParams = await props.searchParams;
+    const query = searchParams?.query || '';
+    const customers = await fetchFilteredCustomers(query);
+
+    return (
+        <div className="w-full">
+            <CustomersTable customers={customers} />
+        </div>
+    );
 }
